f28299d063c9d9e7fff3925eaffef0dc65c5c296
Cesar-Andres-Puma <cesar93puma@gmail.com>
2025-03-22 15:42:00 -0300

    adding form on login

--- views/login/index.php
+++ views/login/index.php
@@ -7,5 +7,17 @@
 </head>
 <body>
     <h1>login</h1>
+    <form action="" style="display: flex; flex-direction: column; width: 200px;">
+        <label for="username">Usuario:</label>
+        <input type="text" name="username" id="username" required>
+        <label for="password">Senha:</label>
+        <input type="password" name="password" id="password" required>
+        <?php
+        if(isset($_GET['error'])){
+            echo '<p style="color: red;">Usuario ou senha invalidos</p>';
+        }
+        ?>
+        <button type="submit">Entrar</button>
+    </form>
 </body>
 </html>
