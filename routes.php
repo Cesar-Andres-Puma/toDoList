f545425ecc17e0f9cadfa256661b404df0a69824
Cesar-Andres-Puma <cesar93puma@gmail.com>
2025-03-22 09:09:47 -0300

    feat: add routes for login, register, dashboard, logout and 404

--- routes.php
+++ routes.php
@@ -1,4 +1,23 @@
 <?php
+ include_once 'init.php';
 
+ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+ $routes = [
+      '/' => 'views/login/index.php',
+      '/register' => 'views/register/index.php',
+      '/dashboard' => 'views/dashboard/index.php',
+      '/logout' => 'controllers/logout.php',
+      '/404' => 'views/error/index.php'
+ ];
+
+ if(array_key_exists($uri, $routes)){
+    include $routes[$uri];
+ }
+ else{
+    http_response_code(404);
+    include $routes['/404'];
+
+ }
 
 ?>
