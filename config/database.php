0a566bf18a3fce3b0712bce87d79c238bf706878
Cesar-Andres-Puma <cesar93puma@gmail.com>
2025-03-21 22:51:25 -0300

    configuring MySQL connection

--- config/database.php
+++ config/database.php
@@ -13,4 +13,15 @@ function loadEnv($file){
 }
 loadEnv(__DIR__ . "../.env");
 
+$host = $_ENV["DB_HOST"];
+$dbname = $_ENV["DB_NAME"];
+$username = $_ENV["DB_USER"];
+$password = $_ENV["DB_PASS"];
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    die("Error ao conectar ao banco de dados:" . $e->getMessage());
+}
 ?>
