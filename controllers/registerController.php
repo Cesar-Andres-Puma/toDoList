77749e86d1920853dc057412ae1af25c60592f65
Cesar-Andres-Puma <cesar93puma@gmail.com>
2025-03-24 23:01:31 -0300

    removing comment

--- controllers/registerController.php
+++ controllers/registerController.php
@@ -10,15 +10,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
     $confirmPassword = $_POST['confirm_password'];
 
-    // Verifica se as senhas coincidem
+
     if ($password !== $confirmPassword) {
-        // Se não coincidirem, exibe uma mensagem de erro
+
         echo 'As senhas não coincidem.';
         exit();
     }
 
-    // Validação adicional: Verifica se o nome de usuário ou o email já estão cadastrados
-    // Exemplo de verificação para nome de usuário
+   
     $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username OR email = :email");
     $stmt->bindParam(':username', $username);
     $stmt->bindParam(':email', $email);
@@ -29,10 +28,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     }
 
-    // Criptografa a senha
+
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    // Insere o novo usuário no banco de dados
+
     $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
     $stmt->bindParam(':username', $username);
     $stmt->bindParam(':email', $email);
@@ -40,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($stmt->execute()) {
         echo 'Usuário cadastrado com sucesso!';
-        // Redirecionar para a página de login, por exemplo
+
         header('Location: /todolist/views/login/index.php');
         exit();
     } else {
